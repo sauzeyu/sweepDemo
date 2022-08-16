4cc46e79d7a2e138e3b2d5cc70e5cf29cacd6052
ouyangcl <ouyangcl@vecentek.com>
2022-08-16 16:05:26 +0800

    修改导入模板文件

--- src/pages/Dashboard/Admin/index.js
+++ src/pages/Dashboard/Admin/index.js
@@ -17,8 +17,8 @@ class Index extends Component {
         style={{ border: 'none' }}
         sandbox="allow-scripts allow-forms allow-same-origin"
         scrolling="auto"
-        // src={'http://172.16.6.107:7001/dkserver-admin/wallboard'}
-        src={'http://10.108.33.50:7001/dkserver-admin/wallboard'}
+        src={'http://172.16.6.107:7001/dkserver-admin/wallboard'}
+        // src={'http://10.108.33.50:7001/dkserver-admin/wallboard'}
         width="100%"
         height="100%"
       />
