2c66ea7b7c676c1a95a11a221daf6453c6010b8f
EdgeYu <770177948@qq.com>
2022-10-18 16:48:12 +0800

    修改 zipkin,admin 地址

--- src/pages/Dashboard/Admin/index.js
+++ src/pages/Dashboard/Admin/index.js
@@ -25,8 +25,7 @@ class Index extends Component {
             style={{ border: 'none' }}
             sandbox="allow-scripts allow-forms allow-same-origin"
             scrolling="auto"
-            // src={'http://172.16.6.107:7001/dkserver-admin/wallboard'}
-            src={'http://10.108.33.50:7001/dkserver-admin/wallboard'}
+            src={'http://172.16.39.21:7001/dkserver-admin/wallboard'}
             width="100%"
             height="100%"
           />
