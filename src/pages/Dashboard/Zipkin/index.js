d289646a9102e063828569f5e6a25d0353ad162f
liujz <liujz@vecentek.com>
2022-10-19 14:36:08 +0800

    '更换logo'

--- src/pages/Dashboard/Zipkin/index.js
+++ src/pages/Dashboard/Zipkin/index.js
@@ -25,8 +25,8 @@ class Index extends Component {
             style={{ border: 'none' }}
             sandbox="allow-scripts allow-forms allow-same-origin"
             scrolling="auto"
-            // src={'http://172.16.13.13:9411/zipkin/'}
-            src={'http://10.108.33.137:9411/zipkin/'}
+            src={'http://172.16.13.13:9411/zipkin/'}
+            // src={'http://10.108.33.137:9411/zipkin/'}
             width="100%"
             height="100%"
           />
