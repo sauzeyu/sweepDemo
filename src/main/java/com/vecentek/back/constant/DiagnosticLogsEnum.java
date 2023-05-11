4259a353158b00ed9dcd531d92aac9bb09b0fdf4
suzeyu <sauzeyu@163.com>
2023-05-11 18:27:34 +0800

    feature: 修改上传诊断日志格式
    1.将诊断日志解耦，去除魔法数字。

--- src/main/java/com/vecentek/back/constant/DiagnosticLogsEnum.java
+++ src/main/java/com/vecentek/back/constant/DiagnosticLogsEnum.java
@@ -11,47 +11,50 @@ public enum DiagnosticLogsEnum {
      * 故障汇总的具体状态
      */
 
-    OPEN_THE_KEY_THERE_IS_A_FROZEN_KEY("04", "开通钥匙","6010","存在已冻结钥匙"),
+    OPEN_KEY_THERE_FROZEN_KEY("04", "开通钥匙","6010","存在已冻结钥匙"),
 
-    OPEN_THE_KEY_PROVISIONING_FAILED("04", "开通钥匙","6011","开通失败"),
-
-
-    REVOKE_KEYS_REPLACE_PHONE_THERE_ARE_NULL_VALUES_IN_THE_PASSING_PARAMETERS("12", "吊销钥匙（更换手机）","5071","传参中存在空值"),
-
-    REVOKE_KEYS_REPLACE_PHONE_THE_KEY_INFORMATION_IS_EMPTY("12", "吊销钥匙（更换手机）","5048","钥匙信息为空"),
-
-    ENTER_VEHICLE_INFORMATION_AFTER_SALES_REPLACEMENT_THERE_ARE_NULL_VALUES_IN_THE_PASSING_PARAMETERS("0D", "录入车辆信息（售后换件）","5071","传参中存在空值"),
-
-    ENTER_VEHICLE_INFORMATION_AFTER_SALES_REPLACEMENT_UPLOAD_DATA_THAT_EXCEEDS_THE_MAXIMUM("0D", "录入车辆信息（售后换件）","5050","上传数据超过最大值"),
-
-    ENTER_VEHICLE_INFORMATION_AFTER_SALES_REPLACEMENT_DUPLICATE_UPLOAD_DATA("0D", "录入车辆信息（售后换件）","5045","上传数据重复"),
-
-    USER_VEHICLE_BINDING_HERE_ARE_NULL_VALUES_IN_THE_PARAMETERS_OR_THE_PARAMETER_FORMAT_IS_INCORRECT("0E", "用户车辆绑定","5071","传参中存在空值或者参数格式不正确"),
-
-    USER_VEHICLE_BINDING_VEHICLE_INFORMATION_DOES_NOT_EXIST("0E", "用户车辆绑定","5004","车辆信息不存在"),
-
-    USER_VEHICLE_BINDING_THE_VEHICLE_INFORMATION_IS_BOUND_TO_THE_OWNER("0E", "用户车辆绑定","5045","车辆信息已绑定车主"),
-
-    OWNER_CANCELLATION_UNBINDING_OF_PEOPLE_AND_VEHICLES_THERE_ARE_NULL_VALUES_IN_THE_PASSING_PARAMETERS("0F", "车主注销（解除人车绑定关系）","5071","传参中存在空值"),
-
-    OWNER_CANCELLATION_UNBINDING_OF_PEOPLE_AND_VEHICLES_USER_VEHICLE_INFORMATION_DOES_NOT_MATCH("0F", "车主注销（解除人车绑定关系）","5046","用户车辆信息不匹配"),
-
-    OWNER_CANCELLATION_UNBINDING_OF_PEOPLE_AND_VEHICLES_VEHICLE_INFORMATION_DOES_NOT_EXIST("0F", "车主注销（解除人车绑定关系）","5004","车辆信息不存在"),
+    OPEN_KEY_PROVISIONING_FAILED("04", "开通钥匙","6011","开通失败"),
 
 
-    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_THERE_ARE_NULL_VALUES_IN_THE_PASSING_PARAMETERS("10", "分享钥匙（用户中心-钥匙平台）","5071","传参中存在空值"),
+    REVOKE_KEYS_REPLACE_PHONE_PARAMETERS_NULL("12", "吊销钥匙（更换手机）","5071","传参中存在空值"),
 
-    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_THE_KEY_TAKES_EFFECT_OR_THE_EXPIRATION_TIME_FORMAT_CANNOT_BE_PARSED("10", "分享钥匙（用户中心-钥匙平台）","5025","钥匙生效或失效时间格式解析失败"),
+    REVOKE_KEYS_REPLACE_PHONE_KEY_NULL("12", "吊销钥匙（更换手机）","5048","钥匙信息为空"),
 
-    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_IT_IS_FORBIDDEN_TO_SHARE_WITH_YOURSELF("10", "分享钥匙（用户中心-钥匙平台）","504B","禁止自己分享给自己"),
 
-    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_THE_KEY_INFORMATION_IS_EMPT("10", "分享钥匙（用户中心-钥匙平台）","5048","钥匙信息为空"),
+    ENTER_VEHICLE_REPLACEMENTTHE_PARAMETERS_NULL("0D", "录入车辆信息（售后换件）","5071","传参中存在空值"),
 
-    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_THE_KEY_STATUS_IS_ABNORMAL_AND_CANNOT_BE_SHARED("10", "分享钥匙（用户中心-钥匙平台）","504A","钥匙状态异常不能分享"),
+    ENTER_VEHICLE_REPLACEMENT_UPLOAD_MAXIMUM("0D", "录入车辆信息（售后换件）","5050","上传数据超过最大值"),
 
-    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_THE_VEHICLE_INFORMATION_IS_EMPTY("10", "分享钥匙（用户中心-钥匙平台）","5004","车辆信息为空"),
+    ENTER_VEHICLE_REPLACEMENT_UPLOAD_REPEAT("0D", "录入车辆信息（售后换件）","5045","上传数据重复"),
 
-    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_BLUETOOTH_INFORMATION_DOES_NOT_HAVE_A_CORRESPONDING_SECONDARY_KEY("10", "分享钥匙（用户中心-钥匙平台）","504E","蓝牙信息没有对应二级密钥"),
+
+    USER_VEHICLE_BINDING_PARAMETERS_NULL_OR_UNFORMAT("0E", "用户车辆绑定","5071","传参中存在空值或者参数格式不正确"),
+
+    USER_VEHICLE_BINDING_VEHICLE_NOT_EXIST("0E", "用户车辆绑定","5004","车辆信息不存在"),
+
+    USER_VEHICLE_BINDING_THE_VEHICLE_BOUND_OWNER("0E", "用户车辆绑定","5045","车辆信息已绑定车主"),
+
+
+    OWNER_CANCELLATION_UNBINDING_PARAMETERS_NULL("0F", "车主注销（解除人车绑定关系）","5071","传参中存在空值"),
+
+    OWNER_CANCELLATION_UNBINDING_USER_VEHICLE_NOT_MATCH("0F", "车主注销（解除人车绑定关系）","5046","用户车辆信息不匹配"),
+
+    OWNER_CANCELLATION_UNBINDING_VEHICLE_INFORMATION_NOT_EXIST("0F", "车主注销（解除人车绑定关系）","5004","车辆信息不存在"),
+
+
+    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_PARAMETERS_NULL("10", "分享钥匙（用户中心-钥匙平台）","5071","传参中存在空值"),
+
+    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_KEY_EFFECT_OR_TIME_UNFORMAT("10", "分享钥匙（用户中心-钥匙平台）","5025","钥匙生效或失效时间格式解析失败"),
+
+    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_SHARE_YOURSELF("10", "分享钥匙（用户中心-钥匙平台）","504B","禁止自己分享给自己"),
+
+    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_KEY_NULL("10", "分享钥匙（用户中心-钥匙平台）","5048","钥匙信息为空"),
+
+    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_KEY_ABNORMAL_AND_CANNOT_SHARED("10", "分享钥匙（用户中心-钥匙平台）","504A","钥匙状态异常不能分享"),
+
+    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_VEHICLE_INFORMATION_IS_EMPTY("10", "分享钥匙（用户中心-钥匙平台）","5004","车辆信息为空"),
+
+    SHARE_KEYS_USER_CENTER_KEY_PLATFORM_BLUETOOTH__NOT_SECONDARY_KEY("10", "分享钥匙（用户中心-钥匙平台）","504E","蓝牙信息没有对应二级密钥"),
 
     ;
 
@@ -92,7 +95,7 @@ public enum DiagnosticLogsEnum {
      * @param business 业务
      * @return {@link String}
      */
-    public static String getBusinessId(String business) {
+    public static String matchBusinessId(String business) {
         for (DiagnosticLogsEnum diagnosticLogsEnum : DiagnosticLogsEnum.values()) {
             if (diagnosticLogsEnum.business.equalsIgnoreCase(business)) {
                 return diagnosticLogsEnum.getBusinessId();
@@ -107,7 +110,7 @@ public enum DiagnosticLogsEnum {
      * @param fault 故障
      * @return {@link String}
      */
-    public static String getFaultId(String fault) {
+    public static String matchFaultId(String fault) {
         for (DiagnosticLogsEnum diagnosticLogsEnum : DiagnosticLogsEnum.values()) {
             if (diagnosticLogsEnum.fault.equalsIgnoreCase(fault)) {
                 return diagnosticLogsEnum.getFaultId();
--- src/main/java/com/vecentek/back/exception/DiagnosticLogsException.java
+++ src/main/java/com/vecentek/back/exception/DiagnosticLogsException.java
@@ -1,5 +1,6 @@
 package com.vecentek.back.exception;
 
+import lombok.Builder;
 import lombok.Data;
 
 /**
@@ -8,10 +9,13 @@ import lombok.Data;
  * @since 2022-04-24 18:15
  */
 @Data
+@Builder
 public class DiagnosticLogsException extends Exception {
     private String businessId;
     private String faultId;
     private Integer code;
+    private String vin;
+    private String userId;
 
 
     public DiagnosticLogsException(String businessId, String faultId) {
@@ -23,5 +27,20 @@ public class DiagnosticLogsException extends Exception {
         this.businessId = businessId;
         this.faultId = faultId;
         this.code = code;
+    }
+
+
+    public DiagnosticLogsException(String businessId, String faultId, Integer code,String vin) {
+        this.businessId = businessId;
+        this.faultId = faultId;
+        this.code = code;
+        this.vin = vin;
+    }
+    public DiagnosticLogsException(String businessId, String faultId, Integer code,String vin, String userId) {
+        this.businessId = businessId;
+        this.faultId = faultId;
+        this.code = code;
+        this.vin = vin;
+        this.userId = userId;
     }
 }
